7fbcebf8eac03c3e45e64684cbdc3ed2077b8e24
Kinshuk Bairagi <me@kinshuk.in>
2016-11-28 18:22:21 +0530

    update deeplink signature to include context and return as Try

--- src/main/scala/com/flipkart/concord/transformer/TURLTransformer.scala
+++ src/main/scala/com/flipkart/concord/transformer/TURLTransformer.scala
@@ -1,6 +1,11 @@
 package com.flipkart.concord.transformer
 
+import scala.util.Try
+
 trait TURLTransformer {
-  def shorten(url: String): String
-  def deeplink(url: String): String
+
+  def shorten(url: String): Try[String]
+
+  def deeplink(url: String, context: Map[String,AnyRef]): Try[String]
+
 }
