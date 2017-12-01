53c79365abb8a3965448b69e7a94a7e333410899
Mimani <saurabh.mimani@flipkart.com>
2017-12-01 11:19:34 +0530

    TGuardrailService Service

--- src/main/scala/com/flipkart/concord/guardrail/TGuardrailService.scala
+++ src/main/scala/com/flipkart/concord/guardrail/TGuardrailService.scala
@@ -1,14 +1,8 @@
 package com.flipkart.concord.guardrail
 
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-
-import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
 trait TGuardrailService {
-
-  def guard(params: AnyRef*): Try[Boolean]
-
-  def entryViaHttp(httpRequest: HttpRequest)(implicit ec: ExecutionContext): Future[HttpResponse]
-
+  def isGuarded(params: AnyRef*): Try[Boolean]
+  def guard(params: AnyRef*): Unit
 }
